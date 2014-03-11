c4cac66e955488ebf30a2c78084b1229adcb0c7b
Han Xiao <han.xiao@cs.helsinki.com>
2014-03-11 20:47:50 +0200

    plotting for MLE&BE improved

--- MLE_and_BE/main.m
+++ MLE_and_BE/main.m
@@ -9,6 +9,17 @@ for i = 1:5
 end
 errors
 
+figure (1)
 plot (1:5, errors (:, 1)', 'r')
 hold on
 plot (1:5, errors (:, 2)', 'b')
+
+title ('MLE and BE error rates for various sequence lengths');
+xlabel ('Sequnce length');
+ylabel ('Error rate');
+
+axis([100 500 0 0.1]);
+
+legend ('Maximum likelihood estimation', 'Bayesian estimation');
+
+saveas (gcf, 'result.png', 'png' )
